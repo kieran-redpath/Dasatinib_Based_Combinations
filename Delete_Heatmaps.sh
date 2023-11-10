c88af0bd5ed447b2b4b347560dd95a06a0399070
Kieran Redpath <axonnfish@gmail.com>
2023-11-10 16:07:10 +1300

    Update before local system reset

---
+++ Delete_Heatmaps.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm Dasatinib_Metagenes_Output/Metagene_Heatmaps/*.png
+
+rm Dasatinib_Metagenes_Output/Metapathway_Heatmaps/*.png
+
+rm Dasatinib_Metagenes_Output/Metapathway_Heatmaps_No_MM/*.png
